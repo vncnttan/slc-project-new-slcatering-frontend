7297a2e62416b55e38620e1f1a5e35bfdb98a40d
vncnttan <vincent.tanjaya.vt@gmail.com>
2024-09-23 12:12:25 +0700

    feat: Footer addition

--- src/routes/(app)/+layout.svelte
+++ src/routes/(app)/+layout.svelte
@@ -136,11 +136,49 @@
     </div>
 </div>
 <div class="h-24 bg-white">
-    <!--    Padding -->
+  <!--    Padding -->
 </div>
 
 
 <slot/>
+
+<!-- Footer -->
+<div class="bg-gradient-to-b from-slate-100 to-gray-100 font-inter mt-16 text-black text-xs">
+  <div class="py-2 responsive-container w-full">
+    <div class="my-2 flex flex-row justify-between w-full">
+      <div class="flex flex-col gap-1">
+        <div class="font-bold text-sm">
+          Contact Support
+        </div>
+        <div class="flex flex-row place-items-center gap-1.5">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-3">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M21.75 6.75v10.5a2.25 2.25 0 0 1-2.25 2.25h-15a2.25 2.25 0 0 1-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0 0 19.5 4.5h-15a2.25 2.25 0 0 0-2.25 2.25m19.5 0v.243a2.25 2.25 0 0 1-1.07 1.916l-7.5 4.615a2.25 2.25 0 0 1-2.36 0L3.32 8.91a2.25 2.25 0 0 1-1.07-1.916V6.75" />
+          </svg>
+          vincent.tanjaya.vt@gmail.com
+        </div>
+        <div class="flex flex-row place-items-center gap-1.5">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-3">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+            <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1 1 15 0Z" />
+          </svg>
+          Jl. Raya Kb. Jeruk No.27, RT.1/RW.9, Kemanggisan, Kec. Palmerah, Kota Jakarta Barat, Daerah Khusus Ibukota Jakarta 11530
+        </div>
+        <div class="flex flex-row place-items-center gap-1.5">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-3">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 0 0 2.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 0 1-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 0 0-1.091-.852H4.5A2.25 2.25 0 0 0 2.25 4.5v2.25Z" />
+          </svg>
+          +62 813-1794-3046
+        </div>
+      </div>
+      <div class="text-xs font-medium flex flex-col place-items-end gap-3">
+        <img src="{slcatering_logo}" alt="SLCatering Logo" class="h-12"/>
+        <div>
+          Developed by NJ23-1 ML23-1 IC23-1
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
 
 
 <style>
