9d48e05bee13dd3360dd7b56d97eaee9716e90ef
vncnttan <vincent.tanjaya.vt@gmail.com>
2024-09-23 13:05:17 +0700

    feat: Logout Button

--- src/routes/(app)/+layout.svelte
+++ src/routes/(app)/+layout.svelte
@@ -71,7 +71,7 @@
 			<img src="{slcatering_logo}" alt="SLCatering Logo" class="h-20 w-64 object-contain" />
 		</div>
 		<div class="hidden md:flex flex-row justify-center place-items-center font-karla">
-			<div class="flex flex-row h-fit relative splace-items-center bg-gray-300 rounded-full text-lg">
+			<div class="flex flex-row h-fit relative place-items-center bg-gray-300 rounded-full text-lg">
 				{#each menus as menu}
 					<button on:click={() => navigateNavbar(menu.link)}
 									class="z-20 w-32 lg:w-40 xl:w-64 rounded-full h-full text-center p-2 flex flex-row gap-2 place-items-center justify-center
@@ -101,10 +101,9 @@
 			<div class="flex flex-col place-items-end">
 				{#if data.user}
 					<div class="flex flex-row gap-2">
-
-                        <span class="text-xl font-extrabold">
-                        {data.user?.username}
-                    </span>
+						<span class="text-xl font-extrabold">
+							{data.user?.username}
+						</span>
 					</div>
 				{:else}
 					<div class="flex flex-row gap-1 mb-1">
@@ -113,25 +112,25 @@
 						</a>
 					</div>
 				{/if}
-				<div class="flex-row gap-1">
-                    <span class="hidden xl:inline">
-                        Mau makan apa hari ini?
-                    </span>
+				<div class="flex-row gap-1 text-sm">
+					<span class="hidden xl:inline">
+							Mau makan apa hari ini?
+					</span>
 					<Typewriter texts={["Budi 🤤", "Nasi Bakar", "Ayam Goreng", "Nasi Hainam", "Burger", "Bakpao"]} />
 				</div>
 			</div>
-			<!--            TODO: Pindahin logoutnya -->
-			<!--            {#if data.user}-->
-			<!--                <form method="POST" action="/logout">-->
-			<!--                    <button class="w-fit h-fit rounded-full bg-gray-200 rotate-180 p-2">-->
-			<!--                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"-->
-			<!--                             stroke="currentColor" class="w-6 h-6">-->
-			<!--                            <path stroke-linecap="round" stroke-linejoin="round"-->
-			<!--                                  d="M8.25 9V5.25A2.25 2.25 0 0 1 10.5 3h6a2.25 2.25 0 0 1 2.25 2.25v13.5A2.25 2.25 0 0 1 16.5 21h-6a2.25 2.25 0 0 1-2.25-2.25V15m-3 0-3-3m0 0 3-3m-3 3H15"/>-->
-			<!--                        </svg>-->
-			<!--                    </button>-->
-			<!--                </form>-->
-			<!--            {/if}-->
+			{#if data.user}
+				<form method="POST" action="/logout">
+					<button class="w-fit rounded-xl bg-orange-sig text-white rotate-180 h-full px-4 py-2.5">
+						<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2"
+								 stroke="currentColor" class="w-6 h-6">
+							<path stroke-linecap="round" stroke-linejoin="round"
+										d="M8.25 9V5.25A2.25 2.25 0 0 1 10.5 3h6a2.25 2.25 0 0 1 2.25 2.25v13.5A2.25 2.25 0 0 1 16.5 21h-6a2.25 2.25 0 0 1-2.25-2.25V15m-3 0-3-3m0 0 3-3m-3 3H15" />
+						</svg>
+<!--						Logout-->
+					</button>
+				</form>
+			{/if}
 		</div>
 	</div>
 </div>
