bf4caec6df40d5750700c216554e8d77d829c58a
vncnttan <vincent.tanjaya.vt@gmail.com>
2024-11-06 10:29:41 +0700

    feat: Close Menu

--- src/routes/product-detail/[id]/+page.svelte
+++ src/routes/product-detail/[id]/+page.svelte
@@ -1,21 +1,26 @@
 <script lang="ts">
-	import { afterNavigate, goto } from '$app/navigation';
+	import { afterNavigate, goto, invalidateAll } from '$app/navigation';
 	import CustomerList from '../../../components/details/CustomerList.svelte';
 	import { base } from '$app/paths';
 	import { page } from '$app/stores';
 	import { onMount } from 'svelte';
-	import { getCateringDetailsById } from '../../../scripts/datas/catering-mutations-and-queries';
-	import { formatPrice } from '../../../scripts/helpers';
+	import {
+		closeCatering,
+		getCateringDetailsById
+	} from '../../../scripts/datas/catering-mutations-and-queries';
+	import { formatPrice, showToast, TOAST_TYPE } from '../../../scripts/helpers';
 	import type { CateringType } from '../../../../types/catering.type';
+	import type { PageData } from './$types';
 
 	let id = $page.params.id;
 	let menu = {} as CateringType;
+	export let data: PageData;
+
 
 	let previousPage: string = base;
 
 	afterNavigate(({ from }) => {
 		previousPage = from?.url.pathname || previousPage;
-
 	});
 
 	onMount(async () => {
@@ -26,6 +31,25 @@
 		if (previousPage.startsWith('/product-detail/') || previousPage.startsWith('/checkout/') || previousPage == '') {
 			previousPage = '/';
 		}
+	}
+
+	async function handleCloseCatering() {
+		if (data.user.username !== menu.created_by?.username && data.user.role !== 'merchant') {
+			showToast('You are not allowed to close this catering', TOAST_TYPE.ERROR);
+		}
+
+		try {
+			if (data.user === null) {
+				showToast('You must login first', TOAST_TYPE.ERROR);
+				return;
+			}
+			await closeCatering(menu.id, data.user?.token);
+			showToast('Menu closed successfully', TOAST_TYPE.SUCCESS);
+			invalidateAll();
+		} catch (e) {
+			showToast('Failed to close menu', TOAST_TYPE.ERROR);
+		}
+
 	}
 </script>
 
@@ -43,9 +67,16 @@
 		class="pb-5 pt-12 my-5 xl:my-0 h-screen flex-grow flex flex-col justify-center place-items-center bg-gray-100 xl:max-w-2xl">
 		<div class="responsive-cols flex flex-col gap-12 font-inter">
 			<div class="flex flex-col md:flex-row xl:flex-col gap-6">
-				{#if menu.imageLink}
-					<img src={menu.imageLink} alt={menu.title} class="h-40 w-40 xl:h-72 xl:w-72 my-2 rounded-xl object-cover" />
-				{/if}
+				<div class="flex flex-col gap-2">
+					<div>
+						<div
+							class="h-2.5 w-2.5 rounded-full {!menu.is_closed ? 'bg-green-600' : 'bg-orange-sig'} me-1 inline-block"></div>
+						{menu.is_closed ? "Closed" : "Open"}
+					</div>
+					{#if menu.imageLink}
+						<img src={menu.imageLink} alt={menu.title} class="h-40 w-40 xl:h-72 xl:w-72 my-2 rounded-xl object-cover" />
+					{/if}
+				</div>
 				<div class="flex flex-col">
 					<div class="text-black text-xs">
 						{menu.date}
@@ -77,6 +108,20 @@
 						<div class="text-xs">
 							* Price may vary depends on your variant and merchant
 						</div>
+
+						{#if data.user.username === menu.created_by?.username && data.user.role === 'merchant' && menu.is_closed === false}
+							<button
+								class="mt-4 py-2 px-4 bg-red-sig hovered-bg-red-sig text-white rounded font-semibold flex flex-row gap-2 w-fit place-items-center"
+								on:click={handleCloseCatering}>
+								<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+										 stroke="currentColor" class="size-4">
+									<path stroke-linecap="round" stroke-linejoin="round"
+												d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
+								</svg>
+
+								Close Catering
+							</button>
+						{/if}
 					</div>
 
 
--- src/scripts/datas/catering-mutations-and-queries.ts
+++ src/scripts/datas/catering-mutations-and-queries.ts
@@ -1,50 +1,61 @@
+import axios from 'axios';
 
-import axios from "axios";
-
-import type { MenuInformationType } from '../../types/menu-information.type';
+import type { MenuInformationType } from '../../../types/menu-information.type';
 
 
 const base_url = import.meta.env.VITE_BACKEND_BASE_URL;
 
 export function createMenu(menu: MenuInformationType, access_token: string) {
-    return axios.post(`${base_url}/catering`, menu, {
-        headers: {
-            "Content-Type": "application/json",
-            "Authorization": `Bearer ${access_token}`
-        }
-    })
+	return axios.post(`${base_url}/catering`, menu, {
+		headers: {
+			'Content-Type': 'application/json',
+			'Authorization': `Bearer ${access_token}`
+		}
+	});
 }
 
+export function closeCatering(id: string, access_token: string) {
+	return axios.patch(`${base_url}/catering`, { catering_id: id }, {
+		headers: {
+			'Content-Type': 'application/json',
+			'Authorization': `Bearer ${access_token}`,
+			'Accept': 'application/json'
+		}
+	});
+}
+
+
 export function getActiveMenus() {
-    return axios.get(`${base_url}/catering?active=true`, {
-        headers: {
-            "Content-Type": "application/json"
-        }
-    })
+	return axios.get(`${base_url}/catering?active=true`, {
+		headers: {
+			'Content-Type': 'application/json'
+		}
+	});
 }
 
 export function getCateringByMerchantId(access_token: string) {
-    return axios.get(`${base_url}/catering?active=false`, {
-        headers: {
-            "Content-Type": "application/json",
-            "Authorization": `Bearer ${access_token}`
-        }
-    })
+	return axios.get(`${base_url}/catering?active=false`, {
+		headers: {
+			'Content-Type': 'application/json',
+			'Authorization': `Bearer ${access_token}`
+		}
+	});
 
 }
 
 export function getCateringDetailsById(id: string) {
-    return axios.get(`${base_url}/catering?id=${id}`, {
-        headers: {
-            "Content-Type": "application/json"
-        }
-    })
+	return axios.get(`${base_url}/catering?id=${id}`, {
+		headers: {
+			'Content-Type': 'application/json'
+		}
+	});
 }
 
-export function getMostPopularMenu(){
-    return axios.get(`${base_url}/leaderboards?menu=true`, {
-        headers: {
-            "Content-Type": "application/json"
-        }
-    })
+export function getMostPopularMenu() {
+	return axios.get(`${base_url}/leaderboards?menu=true`, {
+		headers: {
+			'Content-Type': 'application/json'
+		}
+	});
 }
+
